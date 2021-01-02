89585641910837d25c17bbca845a599e9a9d9855
jhpp114 <jhpp114@uw.edu>
2021-01-01 21:35:55 -0800

    added header to navigate main and shop page

--- React_Ecomm/my-app/src/App.js
+++ React_Ecomm/my-app/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Homepage from './pages/home/homepage.component';
 import ShoppingPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
+
 import {Route, Switch, Link} from 'react-router-dom';
 
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path='/' component={Homepage}/>
         <Route exact path='/shop' component={ShoppingPage}/>
